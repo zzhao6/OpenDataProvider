35099cfb6124c41dc2caa13e3ad1ab6f27cdc4a3
zhe <zzhao@hedgeserv.com>
2016-12-07 09:44:28 -0800

    working on yahoo stock

---
+++ OpenData.Tests/OpenDataStockUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using OpenDataProvider;
+
+namespace OpenData.Tests
+{
+    [TestFixture(Category = "Unit")]
+    public class OpenDataStockUnitTests
+    {
+        private readonly OpenDataStock odStk;
+        public OpenDataStockUnitTests()
+        {
+            odStk = new OpenDataStock();
+        }
+
+        [Test]
+        public async Task ShouldGetStockQuote()
+        {
+            var response = await odStk.GetQuote("MFST");
+            Assert.IsNotNull(response);
+        }
+    }
+}
---
+++ OpenDataProvider/OpenDataStock.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace OpenDataProvider
+{
+    public class OpenDataStock : IOpenData
+    {
+        private readonly WebClient wcWebClient;
+        // TODO: move this to config file
+        //private readonly DateTime sDate = new DateTime(1990, 1, 1); // default start date
+        //private readonly DateTime eDate = new DateTime(2016, 10, 1);    // default end date
+
+        private string yahooStockQuoteBaseUrl1 =
+            $"https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22";
+
+        private string yahooStockQuoteBaseUrl2 = "%22)&format=json&diagnostics=true&&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
+
+        public OpenDataStock()
+        {
+            wcWebClient = new WebClient();
+        }
+
+        public async Task<string> GetQuote(string symbol)
+        {
+            var url = yahooStockQuoteBaseUrl1 + symbol + yahooStockQuoteBaseUrl2;
+            var response = await wcWebClient.DownloadStringTaskAsync(url);
+            return response;
+        }
+
+        public async Task<string> GetHistory(string symbol, DateTime asOfDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetHistory(string symbol, DateTime startDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetSplit(string symbol, DateTime startDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetDividend(string symbol, DateTime startDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
