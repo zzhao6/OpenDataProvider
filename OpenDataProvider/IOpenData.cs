ad325c5552808c0b55d29b32f1defed5951bbee5
zzhao6 <zzhao6@stevens.edu>
2016-12-06 23:52:56 -0800

    init

---
+++ OpenDataProvider/IOpenData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenDataProvider
+{
+    // get data from open source libraries
+
+    // Yahoo Finance API:
+    // stock quote, stock history
+    // option quote
+
+    // fixer.io (ECB):
+    // currency quote
+    // currency history
+
+    public interface IOpenData
+    {
+        Task<string> GetQuote(string symbol);
+        Task<string> GetHistory(string symbol, DateTime asOfDate);
+        Task<string> GetHistory(string symbol, DateTime startDate, DateTime endDate);
+    }
+
+    public interface IOpenDataOption
+    {
+        string GetExpiries(string symbol);
+        string GetQuote(string symbol, DateTime expiry);
+        // For future implementation
+        string GetHistory(string symbol, DateTime expiry, double strike, DateTime asOfDate);
+        string GetHistory(string symbol, DateTime expiry, double strike, DateTime startDate, DateTime endDate);
+    }
+}
