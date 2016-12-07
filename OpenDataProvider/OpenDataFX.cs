435df83ad07fef3a768ae3a5778a310175393674
zzhao6 <zzhao6@stevens.edu>
2016-12-07 00:26:12 -0800

    clean up

--- OpenDataProvider/OpenDataFX.cs
+++ OpenDataProvider/OpenDataFX.cs
@@ -79,29 +79,7 @@ namespace OpenDataProvider
             {
                 throw new ArgumentException("Only support 1 year maximum difference");
             }
-
-            //string url = "";
-            //string sym1 = "";
-            //string sym2 = null;
-            //switch (symbol.Length)
-            //{
-            //    // download list of fx rates for one base symbol
-            //    case 3:
-            //    {
-            //        sym1 = symbol;
-            //        break;
-            //    }
-            //    // download rate for one pair only
-            //    case 6:
-            //    {
-            //        sym1 = symbol.Substring(0, 3);
-            //        sym2 = symbol.Substring(3, 3);
-            //        break;
-            //    }
-            //    default:
-            //        throw new ArgumentException($"Invalid symbol: {symbol}");
-            //}
-
+            
             var response = "";
             for (var dt = startDate; dt <= endDate; dt = dt.AddDays(1))
             {
